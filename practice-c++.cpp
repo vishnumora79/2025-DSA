a0ec7ad11948b29b00ad31ecbe6e50c81b1d72cd
vishnu mora <moravishnu2005@gmail.com>
2025-02-03 10:29:20 +0530

    started DSA again but now this time using C++

---
+++ practice-c++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    const int id = 655;
+    cout << id;
+    cout << "Enter some data:" << "\n";
+    int n;
+    cin >> n; 
+    cout << n;
+
+}
+
